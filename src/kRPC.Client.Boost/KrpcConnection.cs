e3e4d67ba10373e95ab46a6984236e61e33139c4
dhickie <dominichickie@gmail.com>
2026-04-30 12:45:31 +0100

    Stubs first pass

--- src/kRPC.Client.Boost/KrpcConnection.cs
+++ src/kRPC.Client.Boost/KrpcConnection.cs
@@ -1,12 +1,19 @@
 using System.Net;
 using KRPC.Client;
 using KRPC.Client.Services.SpaceCenter;
+using kRPC.Client.Boost.Services;
 
 namespace kRPC.Client.Boost;
 
+/// <summary>
+/// Provides access to boosted kRPC service wrappers.
+/// </summary>
 public class KrpcConnection(string name, IPAddress ip, int rpcPort, int streamPort)
 {
     private readonly Connection _connection = new(name, ip, rpcPort, streamPort);
     
-    public Service SpaceCenter => _connection.SpaceCenter();
+    /// <summary>
+    /// Gets the boosted SpaceCenter service wrapper.
+    /// </summary>
+    public SpaceCenter SpaceCenter => new(_connection.SpaceCenter());
 }
