880785c70aada401d03174afc157bfaa55f06b3d
dhickie <dominichickie@gmail.com>
2026-06-04 21:55:57 +0100

    Add XML comment to RemoteObjectFactory

--- src/kRPC.Client.Boost/Connection/RemoteObjectFactory.cs
+++ src/kRPC.Client.Boost/Connection/RemoteObjectFactory.cs
@@ -1,11 +1,28 @@
 using System.Reflection;
+using kRPC.Client.Boost.Services;
 
 namespace kRPC.Client.Boost.Connection;
 
+/// <summary>
+/// Provides tools for creating instances of remote objects while decoding objects.
+/// </summary>
 internal static class RemoteObjectFactory
 {
+    /// <summary>
+    /// Creates an instance of the specified remote object type.
+    /// </summary>
+    /// <param name="type">The type of remote object</param>
+    /// <param name="connection">The connection multiplexer to use</param>
+    /// <param name="id">The ID of the remote object</param>
+    /// <returns>The remote object</returns>
+    /// <exception cref="ArgumentException">
+    ///     Thrown if the provided type does not inherit from RemoteObject or does not expose a constructor with the correct signature.
+    /// </exception>
     public static object Create(Type type, IConnectionMultiplexer connection, ulong id)
     {
+        if (!type.IsAssignableTo(typeof(RemoteObject)))
+            throw new ArgumentException($"The type {type.Name} does not inherit from {nameof(RemoteObject)}");
+        
         var ctor = type
             .GetTypeInfo()
             .GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)
