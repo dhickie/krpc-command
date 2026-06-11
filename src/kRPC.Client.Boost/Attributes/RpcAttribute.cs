edff7331bba331f6d3b6683ebeeda502953e2cb8
dhickie <dominichickie@gmail.com>
2026-06-11 16:12:23 +0100

    Add Set and Get RPC attributes

--- src/kRPC.Client.Boost/Attributes/RpcAttribute.cs
+++ src/kRPC.Client.Boost/Attributes/RpcAttribute.cs
@@ -1,8 +1,13 @@
 namespace kRPC.Client.Boost.Attributes;
 
-[AttributeUsage(AttributeTargets.Method)]
-internal class RpcAttribute(string service, string procedure) : Attribute
+internal abstract class RpcAttribute(string service, string procedure) : Attribute
 {
     public readonly string Service = service;
     public readonly string Procedure = procedure;
 }
+
+[AttributeUsage(AttributeTargets.Method)]
+internal class GetRpcAttribute(string service, string procedure) : RpcAttribute(service, procedure);
+
+[AttributeUsage(AttributeTargets.Method)]
+internal class SetRpcAttribute(string service, string procedure) : RpcAttribute(service, procedure);
